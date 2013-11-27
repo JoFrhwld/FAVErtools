a84941eebf4c338d417d5ca0fc00d2aa66219cf9
Josef Fruehwald <JoFrhwld@gmail.com>
2013-11-27 17:12:32 +0000

    working on making this work

---
+++ R/plt_vowels.R
@@ -0,0 +1,45 @@
+#' Translate numerical plotnik codes to readable codes
+#' 
+#' This will translate numerical plotnik codes to a readable code. 
+#' Note, this only operates over the part of the numeric code preceding the decimal.
+#' 
+#' @param x a vector of numeric vowel codes
+#' @export
+#' 
+
+plt_vowels <- function(x){
+  vclass.codes = c(
+    `1` = "i",
+    `2` = "e",
+    `3` = "ae",
+    `5` = "o",
+    `6` = "uh",
+    `7` = "u",
+    `*` = "*",
+    `11` = "iy",
+    `12` = "iyF",
+    `21` = "ey",
+    `22` = "eyF",
+    `41` = "ay",
+    `47` = "ay0",
+    `61` = "oy",
+    `42` = "aw",
+    `62` = "ow",
+    `63` = "owF",
+    `72` = "uw",
+    `73` = "Tuw",
+    `82` = "iw",
+    `33` = "aeh",
+    `39` = "aeBR",
+    `43` = "ah",
+    `53` = "oh",
+    `14` = "iyr",
+    `24` = "eyr",
+    `44` = "ahr",
+    `54` = "ohr",
+    `64` = "owr",
+    `74` = "uwr",
+    `94` = "*hr"
+  )
+  vowels <- vowel.codes[as.character(x)]
+}
